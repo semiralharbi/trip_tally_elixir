3fec904c2b9378c01557b1ee30ee822de311254b
semiralh <semir.alharbi@appunite.com>
2024-08-17 20:23:22 +0200

    feat: upload profile picture

---
+++ apps/trip_tally/test/trip_tally/media_test.exs
@@ -0,0 +1,67 @@
+defmodule TripTally.MediaTest do
+  use TripTally.DataCase, async: true
+
+  import TripTally.AccountsFixtures
+
+  alias TripTally.Attachments.Attachment
+  alias TripTally.Media
+
+  @upload_directory "uploads"
+  @fixture_path Path.expand("../support/assets", __DIR__)
+
+  setup do
+    File.mkdir_p!(@upload_directory)
+
+    {:ok, user: user_fixture()}
+  end
+
+  test "upload_file_to_storage/1 uploads the file and returns the correct path", %{user: _user} do
+    file_path = Path.join(@fixture_path, "test.jpg")
+
+    upload = %Plug.Upload{
+      filename: "test.jpg",
+      path: file_path,
+      content_type: "image/jpeg"
+    }
+
+    assert {:ok, destination_path} = Media.upload_file_to_storage(upload)
+
+    assert File.exists?(destination_path)
+    assert String.starts_with?(destination_path, @upload_directory)
+
+    File.rm(destination_path)
+  end
+
+  test "upload_image/2 uploads an image, calculates aspect ratio, and creates an attachment", %{
+    user: user
+  } do
+    file_path = Path.join(@fixture_path, "test.jpg")
+
+    upload = %Plug.Upload{
+      filename: "test.jpg",
+      path: file_path,
+      content_type: "image/jpeg"
+    }
+
+    assert {:ok, %Attachment{} = attachment} = Media.upload_image(user, upload)
+
+    assert attachment.user_id == user.id
+    assert attachment.filename == "test.jpg"
+    assert attachment.type == :image
+    assert attachment.aspect_ratio == 1.4144432586268232
+
+    assert File.exists?(attachment.url)
+
+    File.rm(attachment.url)
+  end
+
+  test "upload_image/2 returns error if the file upload fails", %{user: user} do
+    upload = %Plug.Upload{
+      filename: "non_existent.jpg",
+      path: "non_existent_path",
+      content_type: "image/jpeg"
+    }
+
+    assert {:error, _reason} = Media.upload_image(user, upload)
+  end
+end
